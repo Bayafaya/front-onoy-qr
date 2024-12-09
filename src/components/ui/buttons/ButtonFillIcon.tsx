e60668d75f19c45609795cddac93dbb766c01dba
Bayel Ibraev <bayelibraev1@gmail.com>
2024-12-10 05:32:58 +0600

    btn prime fix

--- src/components/ui/buttons/ButtonFillIcon.tsx
+++ src/components/ui/buttons/ButtonFillIcon.tsx
@@ -1,16 +1,18 @@
+import { useTheme } from "@emotion/react";
 import { IconButton, IconButtonProps } from "@mui/material";
 import { FC } from "react";
 
 const ButtonFillIcon: FC<IconButtonProps> = (props) => {
+  const theme = useTheme();
   return (
     <IconButton
       {...props}
       sx={{
-        bgcolor: "rgba(91, 65, 166, 1)",
+        bgcolor: theme.palette.primary.main,
         border: "1px solid transparent",
         borderRadius: "8px",
         "&: hover": {
-          bgcolor: "rgba(91, 65, 166, 1)",
+          bgcolor: theme.palette.primary.main,
         },
       }}
     >
